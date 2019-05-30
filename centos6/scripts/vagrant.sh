b44042d047afa15799970928be94c74195adf2b8
Bas Meijer <bas.meijer@me.com>
2019-05-30 10:47:04 +0200

    selinux

--- centos6/scripts/vagrant.sh
+++ centos6/scripts/vagrant.sh
@@ -7,6 +7,7 @@ mkdir -pm 700 /home/vagrant/.ssh
 curl -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+restorecon -R -v /home/vagrant/.ssh
 
 # give sudo access (grants all permissions to user vagrant)
 echo "vagrant ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/vagrant
